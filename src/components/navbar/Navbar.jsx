7cf9fe0622959f5d65d64d51c1784e0d7043314c
justina177 <justinaenenche@yahoo.com>
2024-09-29 07:26:33 +0100

    added react-router-dom

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,20 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <div className="logo"> {logo} </div>
+        <div className="logo"> 
+        <img src={logo} alt="veriflux image" />
+        </div>
+        <ul className="nav-links">
+          <li><a href="/home">Home</a></li>
+          <li><a href="/issue-certificate">Issue Certificate</a></li>
+          <li><a href="/verify-certificate">Verify Certificate</a></li>
+          <li><a href="/about-us">About Us</a></li>
+          <li><a href="/contact-us">Contact us</a></li>
+        </ul>
+        <div className="nav-buttons">
+        <button className="login">Login</button>
+        <button className="sign-up">Sign-up</button>
+      </div>
 
     </nav>
   )
